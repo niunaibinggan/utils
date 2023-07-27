9c3378dbfa1cb8278bd1270e2d1f998fb5c9922c
w-yue_wang <w-yue_wang@neusoft.com>
2023-07-27 17:32:22 +0800

    feat： 文件调整

--- rollup.config.js
+++ rollup.config.js
@@ -6,18 +6,18 @@ export default {
   output: [
     {
       format: 'cjs', // 打包为commonjs格式
-      file: 'lib/anran-utils.cjs.js', // 打包后的文件路径名称
-      name: 'medicalUtils' // 打包后的默认导出文件名称
+      file: 'lib/index.cjs.js', // 打包后的文件路径名称
+      name: 'anranUtils' // 打包后的默认导出文件名称
     },
     {
       format: 'esm', // 打包为esm格式
-      file: 'lib/anran-utils.esm.js',
-      name: 'medicalUtils'
+      file: 'lib/index.esm.js',
+      name: 'anranUtils'
     },
     {
       format: 'umd', // 打包为umd通用格式
-      file: 'lib/anran-utils.umd.js',
-      name: 'medicalUtils',
+      file: 'lib/index.umd.js',
+      name: 'anranUtils',
       minifyInternalExports: true
     }
   ],
